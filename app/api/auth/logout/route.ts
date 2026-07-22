5d1de4507435a32e1856f98250804bd623808354
unknown <yandihan@proton.me>
2026-07-22 14:22:08 +0700

    fix: await createClient call in logout route

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,8 +2,8 @@ import { createClient } from '@/lib/supabase/server'
 import { NextResponse } from 'next/server'
 
 export async function POST() {
-  const supabase = createClient()
+  const supabase = await createClient() // <--- Tambahkan await di sini
   await supabase.auth.signOut()
-  
-  return NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_SITE_URL))
+
+  return NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_SITE_URL!))
 }
